1c502ce38d85ac406d82745311911f26755e96be
RendarCP <tjddnr0217@gmail.com>
2019-01-15 19:56:38 +0900

    포폴 사진 삽입 및 설명 삽입

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -3,29 +3,29 @@
         <div>
             <h1 id="pf-title">My PortFolio</h1>
             <div class="pf-content">
-               <img src="../images/logo.png">
-               <p><a href="#">프로젝트명</a></p>
-               <p>프로젝트 내용</p>
+               <img src="../images/calculator.png">
+               <p><a href="#">간단 계산기</a></p>
+               <p>{{ calcontents }}</p>
             </div>
             <div class="pf-content">
-               <img src="../images/logo.png">
-               <p><a href="#">프로젝트명</a></p>
-               <p>프로젝트 내용</p>
+               <img src="../images/dischargecal.png">
+               <p><a href="https://github.com/RendarCP/dischargeCal" target="_blank">전역계산기</a></p>
+               <p>{{ dischargeCal }}</p>
             </div>
             <div class="pf-content">
-               <img src="../images/logo.png">
-               <p><a href="#">프로젝트명</a></p>
-               <p>프로젝트 내용</p>
+               <img src="../images/memo.png">
+               <p><a href="#">간단메모장</a></p>
+               <p>{{ memo }}</p>
             </div>
             <div class="pf-content">
-               <img src="../images/logo.png">
-               <p><a href="#">프로젝트명</a></p>
-               <p>프로젝트 내용</p>
+               <img src="../images/puzzlegame.png">
+               <p><a href="#">퍼즐게임</a></p>
+               <p>{{ puzzlegame }}</p>
             </div>
              <div class="pf-content">
-               <img src="../images/logo.png">
-               <p><a href="#">프로젝트명</a></p>
-               <p>프로젝트 내용</p>
+               <img src="../images/traview.png">
+               <p><a href="#">Traview프로젝트(여행리뷰)</a></p>
+               <p>{{ traview }}</p>
             </div>
         </div>
     </div>
@@ -33,6 +33,15 @@
 
 <script>
 export default {
+    data(){
+      return{
+        calcontents : '간단한 계산기 두 숫자 입력후 계산 ',
+        dischargeCal : '입대일 입력후 전역일 계산기(단축기간 계산 업데이트예정)',
+        memo : '메모장을 벤치마킹하여 제작한 간단 메모장(학교 프로젝트)',
+        puzzlegame : '사진선택후 게임시작후 랜덤으로 섞이는 퍼즐게임',
+        traview : '여행리뷰/후기 사이트 여행일정등을 등록 확인(졸업 프로젝트)'
+      }
+    }
 
 }
 </script>
@@ -42,8 +51,14 @@ export default {
     text-align: center;
 }
 .pf-content{
-    float:left;
-    margin: 30px;
-    border: 2px solid black;
+    float:center;
+    margin: 20px;
+    border-radius: 25px;
+    border: 2px solid #636e72;
+    text-align: center;
+}
+.pf-content img{
+    margin-top: 20px;
+    width:300px;
 }
 </style>
